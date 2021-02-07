f060a9aef6f2e6475b641b5d5f29b39910e60bcf
Martín Coll <martingonzalezcoll@gmail.com>
2021-02-07 20:46:30 -0300

    fix beta reduction

--- interpreter.rust/src/main.rs
+++ interpreter.rust/src/main.rs
@@ -41,11 +41,10 @@ fn read_term_from_file<P: AsRef<Path>>(path: P) -> Result<YamlTerm, Box<dyn Erro
 
 fn main() {
     let file = read_term_from_file(Path::new("example.yaml")).unwrap();
-    let mut term = file.to_term();
+    let term = file.to_term();
 
     println!("Original term: {}", term);
-    term.reduce();
-    println!("After reduction: {}", term);
+    println!("After reduction: {}", term.reduce());
 }
 
 // https://christianpoveda.github.io/blog/untyped-lambda-calculus/
@@ -61,55 +60,41 @@ enum Term {
 }
 
 impl Term {
-    /// Decide if `var` is free in `self`.
-    fn is_free(&self, var: u8) -> bool {
+    /// Replace `var` by `subs` inside `self`
+    fn replace(&self, var: u8, subs: Box<Term>) -> Term {
         match self {
-            Term::Unit => true,
-            Term::Var(var2) => var == *var2,
-            Term::Abs(arg, body) => (var != *arg) && body.is_free(var),
-            Term::App(t1, t2) => t1.is_free(var) || t2.is_free(var),
-        }
-    }
-    /// Replace `var` by `subs` inside `self`. Return `false` if the substitution
-    /// did not take place due to issues with free variables.
-    fn replace(&mut self, var: u8, subs: &Term) {
-        match self {
-            Term::Unit => (),
+            Term::Unit => Term::Unit,
             Term::Var(var2) => {
                 if var == *var2 {
-                    *self = subs.clone();
+                    subs.as_ref().clone()
+                } else {
+                    Term::Var(*var2)
                 }
             }
             Term::Abs(arg, body) => {
                 if var == *arg {
-                    ()
-                } else if subs.is_free(*arg) {
-                    ()
+                    Term::Abs(*arg, Box::new(body.replace(var, subs)))
                 } else {
                     body.replace(var, subs)
                 }
             }
-            Term::App(t1, t2) => {
-                t1.replace(var, subs);
-                t2.replace(var, subs);
-            }
+            Term::App(t1, t2) => Term::App(
+                Box::new(t1.replace(var, subs.clone())),
+                Box::new(t2.replace(var, subs)),
+            ),
         }
     }
 
     /// Reduce `self` if possible.
-    fn reduce(&mut self) -> bool {
+    fn reduce(self) -> Term {
         match self {
             // beta-reduction
-            Term::App(t1, t2) => match t1.as_mut() {
-                Term::Abs(var, body) => {
-                    body.replace(*var, t2);
-                    *self = *body.clone();
-                    true
-                }
-                _ => t1.reduce() || t2.reduce(),
+            Term::App(t1, t2) => match t1.as_ref() {
+                Term::Abs(var, body) => body.replace(*var, t2),
+                _ => Term::App(Box::new(t1.reduce()), Box::new(t2.reduce())),
             },
             Term::Abs(_, term) => term.reduce(),
-            _ => false,
+            t => t,
         }
     }
 }
