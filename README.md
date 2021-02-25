0f96e535af2ff0642e667654aef0ca9603e6b416
Martín Coll <martingonzalezcoll@gmail.com>
2021-02-25 10:46:56 -0300

    repl readme

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ A lambda calculus syntax for YAML
 
 Aside from the mapping from YAML to lambda calculus itself,
 you'll find modules that I implemented to test the expresiveness of the language
+
+## risp-yaml
+
+This module implements lisp on top of the YAML syntax.
+
+### REPL
+
+```bash
+$ cd interpreter.rust
+$ cargo run -q
+risp-yaml >
+a: [+, 40, a]
+
+🔥 RispFunction(...)
+risp-yaml >
+[a, 2]
+
+🔥 Int(42)
+```
--- interpreter.rust/src/main.rs
+++ interpreter.rust/src/main.rs
@@ -1,9 +1,24 @@
-use interpreter::eval_file;
-use std::path::Path;
+use risp::core::create_core_environment;
+use risp::eval::eval;
+use risp_yaml::to_risp;
+use std::io;
 
 fn main() {
-    eval_file(Path::new("examples/e1.yaml"));
-    eval_file(Path::new("examples/e2.yaml"));
-    eval_file(Path::new("examples/e3.yaml"));
-    eval_file(Path::new("examples/e4-abstraction.yaml"));
+    let env = &mut create_core_environment();
+    loop {
+        println!("risp-yaml >");
+        let mut expr = String::new();
+        while io::stdin().read_line(&mut expr).expect("valid code") > 1 {}
+
+        let yaml_expr = serde_yaml::from_str(&expr).expect("invalid yaml line");
+        let risp_expr = to_risp(&yaml_expr);
+        match eval(risp_expr, env) {
+            Ok(res) => println!("🔥 {:?}", res),
+            Err(e) => println!("🙀 {:?}", e),
+        }
+    }
+    // eval_file(Path::new("examples/e1.yaml"));
+    // eval_file(Path::new("examples/e2.yaml"));
+    // eval_file(Path::new("examples/e3.yaml"));
+    // eval_file(Path::new("examples/e4-abstraction.yaml"));
 }
